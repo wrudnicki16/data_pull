65999aa8b774eb3ed4d8e5c1efe831647e7733c0
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-08-08 13:03:37 -0700

    get csv format from json

--- index.js
+++ index.js
@@ -31,15 +31,6 @@ var specialBAT = {
   'doom': 2
 }
 
-const items = json3.items
-const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
-const header = Object.keys(items[0])
-let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-csv.unshift(header.join(','))
-csv = csv.join('\r\n')
-
-console.log(csv)
-
 function getHeroes() {
     console.log("fetching basic hero info...")
 
